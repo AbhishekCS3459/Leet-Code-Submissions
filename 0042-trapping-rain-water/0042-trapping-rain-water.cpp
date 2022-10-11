5d09710d793931ec75f8f9934e77bc607133e2ce
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2022-10-11 08:54:29 +0530

    Time: 51 ms (6.69%), Space: 20.4 MB (42.12%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
-    vector<int>maximum_at_left(vector<int>&arr){
-    int mx=arr[0];
-    int n=arr.size();
-    vector<int>ans;
-   ans.push_back(arr[0]);
-   for (int i = 1; i < n; i++)
-   {
-    mx=max(mx,arr[i]);
-
-    ans.push_back(mx);
-   }
-   
-    return ans;
-}
-    vector<int>maximum_at_right(vector<int>&arr){
-   int n=arr.size();
-    vector<int>ans;
-    int mx=arr[n-1];
-    for (int i = n-1; i >=0; i--)
-    {
-        mx=max(mx,arr[i]);
-        ans.push_back(mx);
-    }
-    reverse(ans.begin(),ans.end());
-    return ans;
-    
-}
-
     int trap(vector<int>& height) {
-         vector<int>water;
-    int sum=0;
-        auto l=maximum_at_left(height);
-        auto r=maximum_at_right(height);
-// water[i]=min(r[i],l[i])-arr[i]
-for (int i = 0; i < height.size(); i++)
-{
-    sum+=min(l[i],r[i])-height[i];
+     stack<int>s;
+        int curr=0;
     
-}
-
-    return sum;
-        
+        int ans=0;
+        while(curr<height.size()){
+            
+            // check for the bigger element than curr element
+            while(!s.empty()&& height[curr]>height[s.top()]){
+                int top=s.top();
+                s.pop();
+                if(s.empty())break;
+              int  distance =curr-s.top()-1;
+               int h = min(height[curr],height[s.top()])-height[top];
+                ans+=h*distance;
+            }
+            
+            s.push(curr++);
+        }
+        return ans;
     }
 };
