3cd1272aa37a27dad489f3d8eab3fcf3d563ba3c
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2022-10-16 19:52:43 +0530

    Added solution - LeetHub

--- Longest K unique characters substring - GFG/longest-k-unique-characters-substring.cpp
+++ Longest K unique characters substring - GFG/longest-k-unique-characters-substring.cpp
@@ -11,32 +11,30 @@ class Solution{
   public:
     int longestKSubstr(string s, int k) {
     // your code here
-   
-    map<char,int>m;
-    int count=m.size();
      int i=0,j=0,mx=INT_MIN;
+     map<char,int>m;
      while(j<s.length()){
-          m[s[j]]++;
-          if(m.size()<k)j++;
-          else if(m.size()==k){
-              mx=max(mx,j-i+1);
-              j++;
-          }
+         m[s[j]]++;
+         if(m.size()<k){
+             j++;
+         }
+         else if(m.size()==k){
+             mx=max(mx,j-i+1);
+             j++;
+         }
          else{
              while(m.size()>k){
-             m[s[i]]--;
-
-              if(m[s[i]]==0)m.erase(s[i]);   
-              i++;
-                 
+                 m[s[i]]--;
+                 if(m[s[i]]==0)m.erase(s[i]);
+                 i++;
              }
-             j++;
+            
+            j++; 
          }
          
      }
-    
-if(mx==INT_MIN)mx=max(mx,-1);
-    return mx;
+     if(mx==INT_MIN)mx=max(mx,-1);
+     return mx;
     }
 };
 
