a5bf65bb8162a42aecd91fc3dc16384eb5b985c2
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2022-10-22 01:50:50 +0530

    Added solution - LeetHub

---
+++ Binary Search - GFG/binary-search.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+  int binary_Search(int arr[], int l, int r, int x)
+{
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+ 
+        // If the element is present at the middle
+        // itself
+        if (arr[mid] == x)
+            return mid;
+ 
+        // If element is smaller than mid, then
+        // it can only be present in left subarray
+        if (arr[mid] > x)
+            return binary_Search(arr, l, mid - 1, x);
+ 
+        // Else the element can only be present
+        // in right subarray
+        return binary_Search(arr, mid + 1, r, x);
+    }
+    return -1;
+}
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+return binary_Search(arr,0,n-1,k);
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++) cin >> arr[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        int found = ob.binarysearch(arr, N, key);
+        cout << found << endl;
+    }
+}
+
+// } Driver Code Ends
