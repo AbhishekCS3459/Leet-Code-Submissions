fa161ab8662b0ebe2fc65d02806923c39a572ccd
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2022-10-22 12:02:47 +0530

    Time: 730 ms (26.40%), Space: 134.8 MB (39.87%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,25 +1,34 @@
 class Solution {
+    // O(N*N) SOLUTION
+//             for(int i=0;i<nums.size()-k+1;i++){
+//             int mx=nums[i];
+            
+//             for(int j=i;j<i+k;j++){
+//                 mx=max(mx,nums[j]);
+//             }
+//             ans.push_back(mx);
+//         }
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-    int i=0,j=0;
-         vector<int>ans;
-        list<int>l;
-        
+        vector<int>ans;
+        deque<int>l;
+        int j=0,i=0;
         while(j<nums.size()){
-       while (l.size() != 0 && l.back() < nums[j])
-            l.pop_back(); 
-        l.push_back(nums[j]);
-
+                  // im am poping frm back because I want to perform the decreasing order     
+            while(!l.empty() && l.back()<nums[j])l.pop_back();
+            l.push_back(nums[j]);
             if(j-i+1<k)j++;
             else if(j-i+1==k){
-                ans.push_back(l.front());
-                if(l.front()==nums[i]) l.pop_front();
+            ans.push_back(l.front());
+                if(nums[i]==l.front())
+                    l.pop_front();
                 i++;
                 j++;
-                
-            }
             
+            }
         }
-             return ans;
+
+        
+        return ans;
     }
 };
