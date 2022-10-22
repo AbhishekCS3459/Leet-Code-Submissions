b30860e49899ffe9d0d7f9ee62482ddb4f1c5d0c
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2022-10-22 10:06:45 +0530

    Added solution - LeetHub

--- Binary Search - GFG/binary-search.cpp
+++ Binary Search - GFG/binary-search.cpp
@@ -10,32 +10,22 @@ using namespace std;
 
 class Solution {
   public:
-  int binary_Search(int arr[], int l, int r, int x)
-{
-    if (r >= l) {
-        int mid = l + (r - l) / 2;
- 
-        // If the element is present at the middle
-        // itself
-        if (arr[mid] == x)
-            return mid;
- 
-        // If element is smaller than mid, then
-        // it can only be present in left subarray
-        if (arr[mid] > x)
-            return binary_Search(arr, l, mid - 1, x);
- 
-        // Else the element can only be present
-        // in right subarray
-        return binary_Search(arr, mid + 1, r, x);
-    }
-    return -1;
-}
     int binarysearch(int arr[], int n, int k) {
         // code here
-return binary_Search(arr,0,n-1,k);
+        int s=1,e=n;
+        int mid;
+        while(s<=e){
+            mid=(s+e)/2;
+            if(arr[mid]==k)return mid;
+            if(arr[mid]>k)e=mid-1;
+            else
+            s=mid+1;
+        }
+        
+    return -1;
         
     }
+    
 };
 
 //{ Driver Code Starts.
