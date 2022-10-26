78bb636b6ee10dea99b3c81e0a3cbd6ae8e7059c
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2022-10-26 17:51:46 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -10,58 +10,47 @@ using namespace std;
 class Solution 
 {
     public:
-    bool is_deserving(int top,vector<vector<int> >& M, int n){
+     bool is_deserving(int top,vector<vector<int> >& M, int n){
        // for deserving it has all row 0
        bool flag=true;
-       for(int i=0;i<n;i++)
-           if((M[top][i]) ||(i!=top && !M[i][top]) || M[top][top] )flag=false;
-         
+       for(int i=0;i<n;i++){
+           if(M[top][i]==1)flag=false;
+          else if((i!=top && M[i][top]==0) || M[top][top]==1)
+          flag=false;
+          
            
-       
+           
+       }
        return flag;
        
-    }
-    bool a_knows_b(int a,int b,vector<vector<int> >& M, int n){
-        if(M[a][b]==1)return true;
-        else
-        return false;
     }
     //Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& M, int n) 
     {
-        // code here 
-        // there are n persons
-        // ith person knows jth persons if M[i][j]==1
         stack<int>s;
         for(int i=0;i<n;i++){
-    // let suppose A,B,C are three persons so by inserting them all we will do below operations
-            s.push(i);  
+            s.push(i);
         }
         while(s.size()!=1){
-            // pop a and b and check weather a know b or vice versa
             int a=s.top();
             s.pop();
             int b=s.top();
             s.pop();
-            if(a_knows_b(a,b,M,n)){
-                // then a cannot be the clebrity
-                s.push(b);
-                
-            }
+            if(M[a][b]==1)
+            // means a knows b so a can't be the clebrity
+            s.push(b);
             else
             s.push(a);
         }
-        // now we have only one element in the stack which may be the possible candidate for the clebrity
-        int possible_candidate_for_clebrity=s.top();
-        //if all the clebrity row are 0 and all column are one except the candidate column
         
-        if(is_deserving(s.top(),M,n))return s.top();
+        // now there is a last person which is the one of the candatite for clebrity
+        
+     if(is_deserving(s.top(),M,n))return s.top();
         else
-        // for(int i=0;i<n;i++){
-        //     if((i!=s.top() )&& (!M[i][s.top()]) || M[s.top()][i] )return -1;
-
-        // }
         return -1;
+    
+           
+
     }
 };
 
