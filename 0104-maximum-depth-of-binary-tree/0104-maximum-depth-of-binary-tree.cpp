d087ba12bef6940a52ff11d44df1e9f5bb98bc6e
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2022-11-04 14:13:07 +0530

    Time: 19 ms (42.63%), Space: 18.9 MB (14.32%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,34 +11,8 @@
  */
 class Solution {
 public:
-   vector<vector<int>>ans;
-    void level_wise_traversal(TreeNode*root){
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            vector<int>v;
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                TreeNode*t=q.front();
-                q.pop();
-                if(t->left!=NULL)q.push(t->left);
-                    if(t->right!=NULL)q.push(t->right);
-                v.push_back(t->val);
-            }
-            ans.push_back(v);
-        }
-    
-    }
     int maxDepth(TreeNode* root) {
-        // Using DFS
-        
-        if(root==NULL)return 0;
-        level_wise_traversal(root);
-        for(auto in : ans){
-            for(auto ele:in)cout<<ele<<" ";
-            cout<<endl;
-        }
-        return ans.size();
+    if(root==NULL)return 0;
+    return 1+max(maxDepth(root->left),maxDepth(root->right));
     }
-
 };
