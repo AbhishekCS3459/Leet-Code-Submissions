3e78e49f27e4210dfb30825c17b1624ae4228a60
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2022-11-06 17:42:17 +0530

    Time: 68 ms (5.62%), Space: 21.1 MB (15.83%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,39 +11,23 @@
  */
 class Solution {
 public:
-    // O{n^2} solution
-    
-//     int height(TreeNode* root){
-//         if(root==NULL)return 0;
-//         return 1+max( height(root->left), height(root->right) );
-//     }
-//     bool isBalanced(TreeNode* root) {
-//         if(root==NULL)return true;
-//        if(abs( height(root->left) - height(root->right) )>1 ) // checking height from the root node
-//            return false;
-        
-//         // now checking height from further node.
-//        if(!isBalanced(root->left) || !isBalanced(root->right)  )return false;
-//         return true;    
-//     }
-    
-    // Modifyed height function
-        int modified_height(TreeNode* root){
+    int height(TreeNode*root){
         if(root==NULL)return 0;
-            
-           int lh=modified_height(root->left);
-            if(lh==-1)return -1;
-            
-       int rh= modified_height(root->right);
-            if(rh==-1)return -1;
-            
-    if(abs(rh-lh)>1)return -1;
-    return 1+max( lh,rh);
+        
+         int left=height(root->left);
+        if(left==-1)return -1;
+        
+        int right=height(root->right);
+        if(right==-1)return -1;
+        
+        if(abs(left-right)>1)return -1;
+        
+        return 1+max(height(root->left),height(root->right));
+        
     }
-    
     bool isBalanced(TreeNode* root) {
         if(root==NULL)return true;
-if (modified_height(root)==-1)return false;
+        if(height(root)==-1)return false;
         else
             return true;
     }
