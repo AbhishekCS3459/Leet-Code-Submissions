944b343c286f36963006e59a22991cf2c76b5deb
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2022-11-06 22:22:20 +0530

    Time: 11 ms (90.09%), Space: 20.6 MB (10.38%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,17 +11,26 @@
  */
 class Solution {
 public:
-    
-    int height(TreeNode*root){
-        if(root==NULL)return 0;
-        return 1+max(height(root->left),height(root->right));
+    pair<int,int>Optimised_function_for_diameter(TreeNode*root){
+          if(root==NULL)return {0,0};
+            pair<int,int> left=Optimised_function_for_diameter(root->left);
+            pair<int,int> right=Optimised_function_for_diameter(root->right);
+        
+        int op1=left.first;
+        int op2=right.first;
+        int combo=left.second+right.second;
+        
+        
+        int diameter=max(max(op1,op2), combo);
+        int max_height=1+max(left.second,right.second);
+        return {diameter,max_height};
     }
     int diameterOfBinaryTree(TreeNode* root) {
         if(root==NULL)return 0;
-        int op1=diameterOfBinaryTree(root->left);
-         int op2=diameterOfBinaryTree(root->right);
-        int combo= height(root->right)+height(root->left);
-       int ans=max(op1,op2);
-        return max(ans,combo);
+        // O[N] solution
+        // there will be three components of the diameter first is the left which contributes to it another is the right ones and the last is the combo of the heights of boths
+      // the hole is comprisies on the pair of the function
+        return Optimised_function_for_diameter(root).first;
+        
     }
 };
