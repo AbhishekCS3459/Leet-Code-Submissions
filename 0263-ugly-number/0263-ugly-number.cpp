d9195f9df675c27cdd7a129fb313ac4a236aad9a
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2022-11-13 12:10:45 +0530

    Time: 7 ms (20.79%), Space: 6 MB (20.58%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     bool isUgly(int n) {
-      // if prime factors of the given number is other than 2,3 or 5 is called ugly number
-       if(n==1)return true;
-        vector<int>v;
-        for(int i=2;i<=sqrt(n);i++){
-            
-            while(n%i==0){
-                v.push_back(i);
-                n=n/i;
-               // v.push_back(n/i);
+        if(n<1)return false;
+        vector<int>primes={2,3,5};
+        for(auto in:primes){
+            while(n%in==0){
+                n/=in;
             }
-            
         }
-        if(n!=1)
-        v.push_back(n);
-
-bool flag=true;
-        
-        for(auto in:v){
-            if(in==2 || in==3 || in==5 )
-                continue;
-            else
-                flag=false;
-        }
-        
-        for(auto in:v){
-            cout<<in<<" ";
-        }
-        return flag;
-        
+        if(n==1)return true;
+        else
+            return false;
     }
 };
