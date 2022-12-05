b3d0cf2671e670d95179070859cda78b67953e63
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2022-12-05 16:45:06 +0530

    Time: 182 ms (58.33%), Space: 52.5 MB (8.33%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
         sort(skill.begin(),skill.end());
-      vector<pair<int,int>>m;
       int n=skill.size();
       
       int k=n/2;
+        long long ans=0;
       int t=skill[0]+skill[n-1];
+      
       for(int i=0;i<k;i++){
         int ct=skill[i]+skill[n-i-1];
         if(t!=ct)return -1;
-        m.push_back( {skill[i],skill[n-i-1]} );
+        else
+        ans+=skill[i]*skill[n-i-1];
+      
       }
-      long long ans=0;
-      for(auto in:m){
-        ans+=in.first*in.second;
-      }
+  
       return ans;
     }
 };
