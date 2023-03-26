8ddefb937d2427352a4f5ed1a2917fc64f4062ce
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2023-03-26 09:36:13 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -8,36 +8,37 @@ class Solution
 	public:
 	//Function to find the shortest distance of all the vertices
     //from the source vertex S.
-    //   bool operator()(pair<int,int>&a,pair<int,int>& b)
-    // {
-    //     // Compare on basis of roll number
-    //     if (a.first ==b.first) {
-    //         return a.second<b.second;
-    //     }
-    //     return a.first<b.first;
-    // }
+      bool operator()(pair<int,int>&a,pair<int,int>& b)
+    {
+        // Compare on basis of roll number
+        if (a.first ==b.first) {
+            return a.second<b.second;
+        }
+        return a.first<b.first;
+    }
     
     
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
         // initialis pq and vector
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        set<pair<int,int>>st;
         vector<int>dist(V,1e9);
         
-        pq.push({0,S});
+        st.insert({0,S});
         
         dist[S]=0;
         
-       while(!pq.empty()){
-           int dist_till_curr=pq.top().first;// 0
-           int value_of_curr=pq.top().second;// 0
-           pq.pop();
+       while(!st.empty()){
+           auto front=*(st.begin());
+           int dist_till_curr= front.first;// 0
+           int value_of_curr=front.second;
+           st.erase(st.begin());
            for(auto in:adj[value_of_curr]){
                int new_nd=in[0]; // 1 
                int new_weight=in[1];  // 9
                if(dist_till_curr+new_weight<dist[new_nd]){   //9+0<int_max
                   dist[new_nd]=dist_till_curr+new_weight;  //dist[1]=9;
-                  pq.push({dist[new_nd],new_nd});   // push {9,1}
+                  st.insert({dist[new_nd],new_nd});   // push {9,1}
                }
            }
        }
