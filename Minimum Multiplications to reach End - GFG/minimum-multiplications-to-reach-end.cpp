66972eeb936fc9c4c2cee541290330212f2c56b2
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2023-04-01 13:19:30 +0530

    Added solution - LeetHub

--- Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.cpp
+++ Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.cpp
@@ -13,15 +13,15 @@ class Solution {
   const int m=100000;
     int minimumMultiplications(vector<int>& arr, int start, int end) {
     //  unordered_set<int> st(arr.begin(), arr.end());
-      priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+      queue<pair<int,int>>q;
         q.push({0,start});
         vector<int>dist(m,INT_MAX);
         vector<int>vis(m,0);
           vis[start]=1;
         while (!q.empty()) {
             // Get the next word and its count from the queue
-            auto count = q.top().first;
-            int currnum = q.top().second;
+            auto count = q.front().first;
+            int currnum = q.front().second;
             q.pop();
             // If we find the end word, return the count
             if (currnum%m == end) {
